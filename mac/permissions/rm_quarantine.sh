7601ff2498e2e558e5a6eeebe300afc15a38e49a
Mitch Faircloth <Mfaircloth67@gmail.com>
2020-05-30 07:33:13 -0400

    added more structure

--- mac/permissions/rm_quarantine.sh
+++ mac/permissions/rm_quarantine.sh
@@ -1,1 +1,14 @@
-xattr -r -d com.apple.quarantine 
+# removes quarantine from application
+
+DIRECTORY=$1
+
+if [ -z ${DIRECTORY} ]; then
+    echo "Pass in a directory to remove from quarantine."
+else
+    attr -r -d com.apple.quarantine $DIRECTORY 
+    if [ $? -eq 0 ]; then
+        echo "Successfully removed Application from Quarantine."
+    else
+        echo "Something went wrong."
+    fi
+fi
